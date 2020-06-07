d2a27f0d622e877bff5bad5e73f0d9c8b509508f
=Bruno Igor <=brigor7@gmail.com>
2020-06-07 16:47:18 -0300

    Escrita de json account inicial atraves do get()

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.listen(3000, () => {
   console.log('API Started!');
 });
 
-app.get('/', (req, res) => {
-  res.send('Seja bem vindo ao API-MY-Bank');
+app.get('/', (_, res) => {
+  fs.readFile('accounts.json', 'utf8', (err, data) => {
+    if (err) {
+      const initialJson = {
+        nextId: 1,
+        accounts: [],
+      };
+      fs.writeFile('accounts.json', JSON.stringify(initialJson), (err) => {
+        console.log(err);
+      });
+    }
+  });
+  res.send('Leitura ok!');
 });
