5bff362452a3243377de4032dd2d27185316340d
=Bruno Igor <=brigor7@gmail.com>
2020-06-07 23:38:51 -0300

    Implementacao do metodo post e gravacao arquivo

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,22 +1,53 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
+  try {
+    fs.readFile('accounts.json', 'utf8', (err, data) => {
+      if (err) {
+        const initialJson = {
+          nextId: 1,
+          accounts: [],
+        };
+        fs.writeFile('accounts.json', JSON.stringify(initialJson), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   console.log('API Started!');
 });
 
-app.get('/', (_, res) => {
+app.post('/account', (req, res) => {
+  let account = req.body;
   fs.readFile('accounts.json', 'utf8', (err, data) => {
-    if (err) {
-      const initialJson = {
-        nextId: 1,
-        accounts: [],
-      };
-      fs.writeFile('accounts.json', JSON.stringify(initialJson), (err) => {
-        console.log(err);
+    if (!err) {
+      // try {
+      let json = JSON.parse(data);
+      account = { id: json.nextId++, ...account };
+      json.accounts.push(account);
+      fs.writeFile('accounts.json', JSON.stringify(json), (err) => {
+        if (err) {
+          console.log('Erro na escrita' + err);
+        } else {
+          res.end;
+        }
       });
+      // } catch (err) {
+      //  console.log(err);
+      //  res.status(400).send({ error: err.message });
+      // }
+    } else {
+      console.log('erro na leitura');
+      res.send('erro na leitura');
     }
   });
-  res.send('Leitura ok!');
+});
+
+app.get('/', (_, res) => {
+  res.send('GET account ok!');
 });
